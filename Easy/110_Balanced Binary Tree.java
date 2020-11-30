1e561982173da65c468210ecda6c28abc0df4ca7
SHUO LI <k1ng@SHUOs-MacBook-Pro.local>
2020-11-29 20:48:01 -0800

    Nov 29

---
+++ Easy/110_Balanced Binary Tree.java
@@ -0,0 +1,75 @@
+/* 
+
+110. Balanced Binary Tree
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+
+Example 2:
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-10^4 <= Node.val <= 10^4
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/****************************************************
+
+Solution 1
+
+****************************************************/
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        boolean[] res = new boolean[1];
+        res[0] = true;
+        check(root, res);
+        return res[0];
+    }
+    
+    private int check(TreeNode node, boolean[] res){
+        if(node == null) return 0;
+        int left = check(node.left, res);
+        int right = check(node.right, res);
+        if(Math.abs(left - right) > 1) res[0] = false;
+        return 1 + Math.max(left, right);
+    }
+}
+
