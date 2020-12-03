26ec28627990b16deec1986d7cdf94b3254a10bc
SHUO LI <ls1991lsok@gmail.com>
2020-12-02 22:02:54 -0800

    Dec 2

---
+++ Easy/111_Minimum Depth of Binary Tree.java
@@ -0,0 +1,91 @@
+/* 
+
+111. Minimum Depth of Binary Tree
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 10^5].
+-1000 <= Node.val <= 1000
+
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/****************************************************
+
+Solution 1 --- BFS
+
+****************************************************/
+
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        int depth = 0;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            depth++;
+            int curSize = queue.size();
+            for(int i = 0; i < curSize; i++){
+                TreeNode cur = queue.poll();
+                if(cur.left == null && cur.right == null) return depth; 
+                if(cur.left != null) queue.offer(cur.left);
+                if(cur.right != null) queue.offer(cur.right);
+            }
+        }
+        
+        return depth;
+    }
+}
+
+/****************************************************
+
+Solution 1 --- Recursive
+
+****************************************************/
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        
+        return 1 + (Math.min(left, right) > 0? Math.min(left, right) : Math.max(left, right));
+    }
+}
+
---
+++ Easy/112_Path Sum.java
@@ -0,0 +1,84 @@
+/* 
+
+112. Path Sum
+
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/****************************************************
+
+Solution 1 --- Recursive
+
+****************************************************/
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null && root.val == sum) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
+/****************************************************
+
+Solution 1 --- DFS
+
+****************************************************/
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        
+        Deque<TreeNode> nodes = new ArrayDeque<>();
+        Deque<Integer> nums = new ArrayDeque<>();
+        nodes.push(root);
+        nums.push(sum);
+        while(!nodes.isEmpty()){
+            TreeNode curNode = nodes.pop();
+            int curSum = nums.pop();
+            if(curNode.left == null && curNode.right == null && curNode.val == curSum) return true;
+            if(curNode.right != null){
+                nodes.push(curNode.right);
+                nums.push(curSum - curNode.val);
+            }
+            if(curNode.left != null){
+                nodes.push(curNode.left);
+                nums.push(curSum - curNode.val);
+            }
+        }
+        
+        return false;
+    }
+}
+
